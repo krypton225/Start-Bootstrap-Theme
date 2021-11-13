e820ca855728c6f6a4b3c9da3621ca2bc4134b85
Krypton <k.mohamed.11.98@gmail.com>
2021-11-13 20:24:46 +0200

    This is with toggle menu.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ window.addEventListener("scroll", function () {
   const goToTop = document.querySelector("#goToTop");
   goToTop.classList.toggle("block", window.scrollY > 300);
 });
+
+function toggleMenu() {
+  var toggle = document.querySelector(".toggle");
+  toggle.classList.toggle("active");
+
+  var menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+}
